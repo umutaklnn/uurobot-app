b8cbb81a68531c124db82cee6fd7fb8dad0435ae
emirefek <emirefek@gmail.com>
2022-10-18 23:25:24 +0300

    TEST

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Umut Edit <code>src/App.tsx</code> and save to test HMR
+          Umut adwdawdawdEdit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
