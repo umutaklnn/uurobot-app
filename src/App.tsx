9c2dbfbbc1632bb2523ba61ff7b7fbc5082ef30f
emirefek <emirefek@gmail.com>
2022-10-27 22:43:53 +0300

    About page and footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./assets/css/App.css";
+import Footer from "./components/Footer";
 
 import Header from "./components/Header";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
